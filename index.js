3079b017a0b7c3282debb0fd8e705e1124bca9ad
Alhadis <gardnerjohng@gmail.com>
2015-11-06 04:12:16 +1100

    Use Babel 6 transformers to transpile modules

--- index.js
+++ index.js
@@ -5,28 +5,14 @@
 global.util         = require("util");
 global.fs           = require("fs");
 global.process      = require("process");
-global.HTTP			= require("http");
+global.HTTP         = require("http");
 global.queryString  = require("querystring");
 
 /** Load top-level NPM modules */
-global.Babel        = require("babel");
+global.Babel        = require("babel-core/register");
 global.JSDom        = require("jsdom");
 global.fetch        = require("node-fetch");
 global.CookieJar    = require("tough-cookie");
 
-
-
-/** Top-level wrapper to enable ES6 module importing/exporting */
-const ES6ModuleLoader		 = require("es6-module-loader").System;
-ES6ModuleLoader.transpiler	 = "babel";
-ES6ModuleLoader.babelOptions = {
-	whitelist: [
-		"es6.destructuring",
-		"es6.modules",
-		"es6.parameters",
-		"es6.regex.sticky",
-		"es6.regex.unicode"
-	]
-};
-ES6ModuleLoader.import("./src/main.js")
-	.catch(e => { console.error(e); })
+/** Start the program */
+require("./src/main.js");
